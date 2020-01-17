3e9511c37fbcfea0ff137c0309109bef8b3bb7f0
Abhinav Mulagada <abhinav.abhi96@gmail.com>
2020-01-17 12:00:28 -0800

    can move files to destination folder

--- fileMover.py
+++ fileMover.py
@@ -1,3 +1,27 @@
-#testing github integration with atom
-# now I am testing branching 
-# now i am pushing from vscode
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+# import watchdog
+
+import os
+import json
+import time
+ 
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            os.rename(src, new_destination)
+
+folder_to_track = "/Users/Abhinav/Desktop/oldFolder"
+folder_destination = "/Users/Abhinav/Desktop/newFolder"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive = True)
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
